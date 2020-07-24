fefe35aa8fe791c09adb24e0045032367a6cac68
LGugs <lgpa@icomp.ufam.edu.br>
2020-07-24 13:20:15 -0400

    att commentary

--- db_apis/empresas.js
+++ db_apis/empresas.js
@@ -7,6 +7,7 @@ async function find(emp) {
 
     const juncao = {};
 
+    // se especificar um CNPJ, retornará informações mais detalhadas
     if (emp.emp_cnpj){
         
         juncao.emp_cnpj = emp.emp_cnpj;
@@ -14,7 +15,7 @@ async function find(emp) {
         query += `, emp_porte "porte", emp_opt_simples "opt_simples", emp_logradouro "logradouro", emp_bairro "bairro" from pss.csuf_empresa\nwhere emp_cnpj = :emp_cnpj`;
 
     }else{
-
+        // senão, retornará uma lista de empresas
         const limit = (emp.limit > 0) ? emp.limit : 30; // limita 30 resultados por padrão - // limit não funciona no 11g só no 12c pra cima. Portanto é para usar ROWNUM
 
         juncao.row_limit = limit;
