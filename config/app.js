d78951ba1fecff95f0bf93b7182810ea83df278d
LGugs <lgpa@icomp.ufam.edu.br>
2020-08-07 08:12:23 -0400

    just a comment... :P

--- config/app.js
+++ config/app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv'); // faz com que leia o arquivo .env
 
 dotenv.config();
 
+// AUTH.HOST é o servidor de autenticação a ser inserido
 module.exports = {
     port: process.env.HTTP_PORT || 3000,
     auth_host: process.env.AUTH_HOST
